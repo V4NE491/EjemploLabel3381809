a87c6b612b2dc83ff1812f60a0e89407ea7da2ab
Indel <Indel@DESKTOP-BEC38OO>
2024-06-27 14:20:58 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace EjemploLabel3381809
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+          
+        }
+    }
+
+}
